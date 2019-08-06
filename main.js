60433000c0de1e7dbd5c53e23b380813a4c65db9
Adam Bien <adam.bien94@gmail.com>
2019-08-06 21:52:15 +0200

    minor changes

--- main.js
+++ main.js
@@ -21,29 +21,20 @@ let trans = () => {
   }, 750);
 };
 
-// const moreBtn = document.getElementById("more-btn");
+// const links = document.querySelectorAll(".nav__item__link");
 
-// const mySvg = document.querySelector(".my-svg");
-
-// const startPath =
-//   "M0 0H459V46L416.856 64.6826C381.248 80.4672 341.482 84.2764 303.526 75.5383L269.153 67.6252C238.899 60.6602 207.358 61.6386 177.594 70.4652L129.031 84.8665C95.7234 94.7441 60.2685 94.7715 26.9452 84.9454L0 77V0Z";
-
-// const endPath =
-//   "M0 0H459V46L412.968 239.845C399.415 296.917 320.453 302.634 298.82 248.109L266.994 167.889C248.809 122.052 186.163 116.506 160.207 158.435L144.007 184.604C117.829 226.892 54.5209 220.799 36.8885 174.293L0 77V0Z";
-
-// moreBtn.addEventListener("click", () => {
-//   document.querySelector(".header").classList.toggle("header--opened");
-//   const timeline = anime.timeline({
-//     duration: 10000,
-//     easing: "easeOutExpo"
+// for (const link of links) {
+//   link.addEventListener("click", () => {
+//     activeLink(link.hash);
 //   });
+// }
 
-//   timeline.add({
-//     targets: ".pr",
-//     d: [
-//       {
-//         value: endPath
-//       }
-//     ]
-//   });
-// });
+// function activeLink(target) {
+//   for (const link of links) {
+//     if (link.hash === target) {
+//       link.classList.add("nav__item__link--active");
+//     } else {
+//       link.classList.remove("nav__item__link--active");
+//     }
+//   }
+// }
